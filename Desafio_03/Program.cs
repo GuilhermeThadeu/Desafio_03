6c1fffb7594b1cf87b48797a551a80bedda0befb
Guilherme Thadeu <guilhermetadeuh@hotmail.com>
2021-12-08 14:58:12 -0300

    Criei um public class Aluno, para salvar os nomes e as notas inseridas no programa e depois usei o "for" para saber a quantidade de alunos informada para eu botar o nome e nota de acordo com o numero estabelecido.

---
+++ Desafio_03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Desafio_03
+{
+    class Program
+    {
+        public class Aluno
+        {
+            public string Nome;
+            public int Nota;
+        }
+        static void Main(string[] args)
+        {
+            int quant;
+            Aluno MaiorAluno = new Aluno();
+            MaiorAluno.Nota = 0;
+            Console.WriteLine($"Informe a quantidade de alunos:");
+            quant = int.Parse(Console.ReadLine());
+            Aluno aluno = new Aluno();
+            
+            for (int i = 0; i < quant; i++)
+            {
+                Console.WriteLine($"Informe o nome do aluno:");
+                aluno.Nome = Console.ReadLine();
+
+                Console.WriteLine($"Informe a nota do aluno:");
+                aluno.Nota = int.Parse(Console.ReadLine());
+
+                if (MaiorAluno.Nota < aluno.Nota)
+                {
+                    MaiorAluno = aluno;
+                }
+                
+            }
+           
+            Console.WriteLine($"Aluno com maior nota:{aluno.Nome} e sua nota é: {aluno.Nota}");
+        }
+
+
+    }
+}
+
